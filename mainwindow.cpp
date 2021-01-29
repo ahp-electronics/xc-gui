075dc08b511a369b4c0ac8584b8195eb4e3d7dc6
Ilia Platone <info@iliaplatone.com>
2021-01-30 00:05:49 +0100

    Fixed some minor bugs

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,75 +13,59 @@ void MainWindow::ReadThread(MainWindow *wnd)
     wnd->threadsRunning = true;
     QDateTime start = QDateTime::currentDateTimeUtc();
     ahp_xc_packet* packet = wnd->createPacket();
-    int stepping = 1000000.0/ahp_xc_get_packettime();
-    int *auto_lag = new int[packet->n_lines];
-    bool skip[packet->n_lines];
-    int average_value = 1;
-    int averaged = average_value;
-    double coherence = 0;
-    double crosscoherence = 0;
     while(wnd->threadsRunning) {
         QThread::usleep(ahp_xc_get_packettime());
-        if(wnd->getMode() == Autocorrelator)
-        {
+        wnd->getGraph()->Update();
+        switch (wnd->getMode()) {
+            case Counter:
+            if(!ahp_xc_get_packet(packet)) {
+                QDateTime now = QDateTime::currentDateTimeUtc();
+                double diff = (double)start.msecsTo(now)/1000.0;
+                for(int x = 0; x < packet->n_lines; x++) {
+                    Line * line = wnd->Lines[x];
+                    QLineSeries *dots = line->getDots();
+                    QLineSeries *average = line->getAverage();
+                    if(diff > (double)wnd->getTimeRange())
+                        for(int d = 0; d < dots->count(); d++)
+                            if(dots->at(d).x()<diff-(double)wnd->getTimeRange())
+                                dots->remove(d);
+                    for(int d = 0; d < average->count(); d++)
+                        if(average->at(d).x()<diff-(double)wnd->getTimeRange())
+                            average->remove(d);
+                    if(line->isActive()) {
+                            dots->append(diff, packet->counts[x]);
+                            average->append(diff, packet->counts[x]);
+                            wnd->getGraph()->Update();
+                    } else {
+                        dots->clear();
+                        average->clear();
+                    }
+                }
+            } else {
+                ahp_xc_enable_capture(0);
+                ahp_xc_enable_capture(1);
+            }
+            break;
+        case Autocorrelator:
             for(int x = 0; x < packet->n_lines; x++) {
                 Line * line = wnd->Lines[x];
                 if(line->isActive())
                     line->stackCorrelations();
                 else
                     line->clearCorrelations();
-                wnd->getGraph()->Update();
             }
-            continue;
-        } else if(wnd->getMode() == Crosscorrelator)
-        {
+            break;
+        case Crosscorrelator:
             for(int x = 0; x < packet->n_baselines; x++) {
                 Baseline * b = wnd->Baselines[x];
                 if(b->isActive())
                     b->stackCorrelations((int)wnd->Lines[b->getLine1()]->getYScale()>(int)wnd->Lines[b->getLine2()]->getYScale() ? wnd->Lines[b->getLine1()]->getYScale() : wnd->Lines[b->getLine2()]->getYScale());
                 else
                     b->clearCorrelations();
-                wnd->getGraph()->Update();
             }
-            continue;
-        } else if(!ahp_xc_get_packet(packet)) {
-            QDateTime now = QDateTime::currentDateTimeUtc();
-            double diff = (double)start.msecsTo(now)/1000.0;
-            for(int x = 0; x < packet->n_lines; x++) {
-                Line * line = wnd->Lines[x];
-                QLineSeries *dots = line->getDots();
-                QLineSeries *average = line->getAverage();
-                switch (wnd->getMode()) {
-                    case Counter:
-                    if(diff > (double)wnd->getTimeRange())
-                    for(int d = 0; d < dots->count(); d++)
-                        if(dots->at(d).x()<diff-(double)wnd->getTimeRange())
-                            dots->remove(d);
-                    for(int d = 0; d < average->count(); d++)
-                        if(average->at(d).x()<diff-(double)wnd->getTimeRange())
-                            average->remove(d);
-                    if(line->isActive()) {
-                        if(!skip[x]) {
-                            dots->append(diff, packet->counts[x]);
-                            average->append(diff, packet->counts[x]);
-                            wnd->getGraph()->Update();
-                        }
-                        skip[x] = false;
-                    } else {
-                        dots->clear();
-                        average->clear();
-                        skip[x] = true;
-                        auto_lag[x] = 2;
-                        ahp_xc_set_lag_auto(x, auto_lag[x]);
-                    }
-                        break;
-                    default:
-                        break;
-                }
-            }
-        } else {
-            ahp_xc_enable_capture(0);
-            ahp_xc_enable_capture(1);
+            break;
+        default:
+            break;
         }
     }
     wnd->freePacket(packet);
