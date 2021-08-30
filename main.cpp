ed8afc2cbb3b924f96c058654a1bbec8a42d4c46
Ilia Platone <info@iliaplatone.com>
2021-08-30 21:49:55 +0200

    Added WinMain entry point

--- main.cpp
+++ main.cpp
@@ -2,9 +2,18 @@
 
 #include <QApplication>
 
+#ifdef _WIN32
+#include <windows.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
+{
+    int argc = 0;
+    char **argv = {NULL};
+    QApplication a(argc, argv);
+#else
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+#endif
     MainWindow w;
     w.show();
     a.setWindowIcon(QIcon(":/icons/icon.ico"));
