6101b8751c5c765a47ab2519dc0b8e930ca0fa7c
Ilia Platone <info@iliaplatone.com>
2022-03-02 00:30:19 +0100

    Add pre-installed check in windows installer

--- xc-gui.iss.cmake
+++ xc-gui.iss.cmake
@@ -23,3 +23,33 @@ Source: "../bin/{#TargetName}32/*"; DestDir: "{app}"; Check: not Is64BitInstallM
 [Icons]
 Name: "{group}\{#MyAppName}"; Filename: "{app}\{#TargetName}.exe"
 Name: "{commondesktop}\{#MyAppName}"; Filename: "{app}\{#TargetName}.exe"
+
+[Code]
+function VersionInstalled(const ProductID: string): String;
+var
+  UninstallExe: String;
+  UninstallRegistry: String;
+begin
+    // Create the correct registry location name, which is based on the AppId
+    UninstallRegistry := ExpandConstant('Software\Microsoft\Windows\CurrentVersion\Uninstall\' + ProductID + '_is1');
+    // Check whether an extry exists
+    RegQueryStringValue(HKLM, UninstallRegistry, 'UninstallString', UninstallExe);
+    Result := UninstallExe;
+end;
+
+procedure CurStepChanged(CurStep: TSetupStep);
+var
+  ResultCode: Integer;
+  UninstallExe: String;
+begin
+  if (CurStep = ssInstall) then // Install step has started
+        begin
+        UninstallExe := VersionInstalled('{#SetupSetting("AppId")}');
+        if (not (UninstallExe = '')) then
+        begin
+          MsgBox('Setup will now remove the previous version.', mbInformation, MB_OK);
+          Exec(RemoveQuotes(UninstallExe), ' /SILENT', '', SW_SHOWNORMAL, ewWaitUntilTerminated, ResultCode);
+          sleep(1000);    //Give enough time for the install screen to be repainted before continuing
+        end;
+  end;
+end;
