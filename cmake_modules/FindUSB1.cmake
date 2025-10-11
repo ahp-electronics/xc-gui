8dbe0fa74f3a178c8bcda29993506389d06375ab
Ilia Platone <info@iliaplatone.com>
2025-10-12 01:04:58 +0200

    Update FindUSB1.cmake

--- cmake_modules/FindUSB1.cmake
+++ cmake_modules/FindUSB1.cmake
@@ -1,38 +1,79 @@
-# - Try to find the freetype library
-# Once done this defines
+# - Try to find libopenUSB1
+# Once done this will define
 #
-#  USB1_FOUND - system has USB1
-#  USB1_INCLUDE_DIR - the USB1 include directory
-#  USB1_LIBRARIES - Link these to use USB1
-
-# Copyright (c) 2006, 2008  Laurent Montel, <montel@kde.org>
+#  USB1_FOUND - system has libopenUSB1
+#  USB1_INCLUDE_DIRS - the libopenUSB1 include directories
+#  USB1_LIBRARIES - Link these to use libopenUSB1
+#  USB1_DEFINITIONS - Compiler switches required for using libopenUSB1
 #
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#  USB1_HAS_USB1_VERSION - defined when libopenUSB1 has USB1_get_version()
 
+#=============================================================================
+# Copyright (c) 2022 Ilia Platone <info@iliaplatone.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#=============================================================================
 
-if (USB1_INCLUDE_DIR AND USB1_LIBRARIES)
+find_path(USB1_INCLUDE_DIRS
+  NAMES
+    libusb.h
+  PATH_SUFFIXES
+    libusb-1.0
+  HINTS
+    ${USB1_INCLUDE_DIRS}
+)
 
-  # in cache already
-  set(USB1_FOUND TRUE)
+find_library(USB1_LIBRARIES
+  NAMES
+    usb-1.0
+  HINTS
+    ${USB1_LIBRARY_DIRS}
+)
 
-else (USB1_INCLUDE_DIR AND USB1_LIBRARIES)
-  IF (NOT WIN32)
-    # use pkg-config to get the directories and then use these values
-    # in the FIND_PATH() and FIND_LIBRARY() calls
-    find_package(PkgConfig)
-    pkg_check_modules(PC_LIBUSB libusb-1.0)
-  ENDIF(NOT WIN32)
+set(USB1_INCLUDE_DIRS ${USB1_INCLUDE_DIR})
+set(USB1_LIBRARIES ${USB1_LIBRARY})
 
-  FIND_PATH(USB1_INCLUDE_DIR libusb.h
-    PATHS ${PC_LIBUSB_INCLUDEDIR} ${PC_LIBUSB_INCLUDE_DIRS})
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(USB1
+  FOUND_VAR
+    USB1_FOUND
+  REQUIRED_VARS
+    USB1_LIBRARIES
+    USB1_INCLUDE_DIRS
+  VERSION_VAR
+    USB1_VERSION
+)
 
-  FIND_LIBRARY(USB1_LIBRARIES NAMES usb-1.0
-    PATHS ${PC_LIBUSB_LIBDIR} ${PC_LIBUSB_LIBRARY_DIRS})
+mark_as_advanced(USB1_INCLUDE_DIRS USB1_LIBRARIES)
 
-  include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(USB1 DEFAULT_MSG USB1_LIBRARIES USB1_INCLUDE_DIR)
-
-  MARK_AS_ADVANCED(USB1_INCLUDE_DIR USB1_LIBRARIES)
-
-endif (USB1_INCLUDE_DIR AND USB1_LIBRARIES)
+if(USB1_FOUND)
+  include(CheckCXXSourceCompiles)
+  include(CMakePushCheckState)
+  cmake_push_check_state(RESET)
+  set(CMAKE_REQUIRED_INCLUDES ${USB1_INCLUDE_DIRS})
+  set(CMAKE_REQUIRED_LIBRARIES ${USB1_LIBRARIES})
+  check_cxx_source_compiles("#include <USB1.h>
+    int main() { USB1_get_version(); return 0; }" USB1_HAS_USB1_VERSION)
+  cmake_pop_check_state()
+endif()
