c68ca3b09c6c52822cb60023144514afcd60deae
Ilia Platone <info@iliaplatone.com>
2025-12-21 23:42:37 +0100

    update update_firmware.sh

--- update_firmware.sh
+++ update_firmware.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 url=$1
 curl "${url}?product=xc*" | jq .data | tr -d '"' | base64 -d | tr -s ',' '\n' | cut -d '/' -f 2 | cut -d '-' -f 1 | while read line; do curl "${url}?product=$line&download=on" -o $line.json; done
-head -n 2 app.rc > app.rc
-head -n 6 resource.qrc > resource.qrc
 i=1
 for file in *.json; do echo "<file>"$file"</file>" >> resource.qrc;
 echo "IDI_DATA$i               TEXT        DISCARDABLE            \"$file\"" >> app.rc
