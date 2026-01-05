4a81173dc6be61a1743669a1e757fa6c9838606c
Ilia Platone <info@iliaplatone.com>
2026-01-05 13:54:32 +0100

    Use wget instead of curl

--- update_firmware.sh
+++ update_firmware.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 url=$1
-curl --resolve "iliaplatone.com:443:192.71.211.119" "${url}?product=xc*" | jq .data | tr -d '"' | base64 -d | tr -s ',' '\n' | cut -d '/' -f 2 | cut -d '-' -f 1 | while read line; do curl --resolve "iliaplatone.com:443:192.71.211.119" "${url}?product=$line&download=on" -o $line.json; done
+wget "${url}?product=xc*" -O - | jq .data | tr -d '"' | base64 -d | tr -s ',' '\n' | cut -d '/' -f 2 | cut -d '-' -f 1 | while read line; do curl --resolve "iliaplatone.com:443:192.71.211.119" "${url}?product=$line&download=on" -o $line.json; done
 i=1
 echo "IDI_ICON1               ICON        DISCARDABLE            \"icon.ico\"" > app.rc
 
